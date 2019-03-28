960199fc9ff6e88359305a53bfe9b6e026379be0
nguyenngocbang <bang270197@gmail.com>
2019-03-28 14:11:50 +0700

    sudungtoantutrongJS

---
+++ SudungtoantutrongJS.js
@@ -0,0 +1,8 @@
+let inputWith;
+let inputheight;
+inputWith=prompt("Enter the width");
+inputheight=prompt("Enter the height");
+let w = parseFloat(inputWith);
+let h = parseFloat(inputheight);
+let area= w+ h;
+document.write("the area is: "+area);
